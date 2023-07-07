03e5eed21e657990f1e3eca26148aad78b5c862a
fico <sunmaybo@gmail.com>
2023-07-07 11:27:06 +0800

    修改zctl解决上线问题

--- zctl/front/release/release_test.go
+++ zctl/front/release/release_test.go
@@ -8,5 +8,5 @@ func TestDecrypt(t *testing.T) {
 	Delay("format", ".", "", false, "", "", "", true, false)
 }
 func TestEncrypt(t *testing.T) {
-	t.Log(EncryptByAes("xbb123:::", []byte("LTAI5t89dnZgsEC99idBKb67-5eYCj3sKYLeyScX0kcfUrKDJ1kUIpB")))
+	t.Log(EncryptByAes("xbb123:::", []byte("")))
 }
