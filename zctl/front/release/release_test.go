f4b4fe864f10d1cc38bc2d2776f4c7358e605b2b
fico <sunmaybo@gmail.com>
2023-11-20 15:33:55 +0800

    调整分页逻辑

--- zctl/front/release/release_test.go
+++ zctl/front/release/release_test.go
@@ -1,6 +1,9 @@
 package release
 
 import (
+	"fmt"
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"os"
 	"testing"
 )
 
@@ -9,4 +12,21 @@ func TestDecrypt(t *testing.T) {
 }
 func TestEncrypt(t *testing.T) {
 	t.Log(EncryptByAes("xbb123:::", []byte("")))
+}
+
+func TestUploadCompanyWebsite(t *testing.T) {
+	endpoint := "oss-cn-beijing.aliyuncs.com"
+	client, err := oss.New(endpoint, "LTAI5tELM1GW19ezt88VCxVG", "NPZCY9FLUWcNF9p6MhAhukYcUQsCZX")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(-1)
+	}
+
+	// 填写存储空间名称，例如examplebucket。
+	bucket, err := client.Bucket("xbanban-site")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(-1)
+	}
+	uploadDirectoryFileTree(bucket, "/Users/fico/Desktop/dist/", "")
 }
